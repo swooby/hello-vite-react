468b52582cd2d671910f2e1eaf31e6546c8444c4
Paul Peavyhouse <pv@swooby.com>
2025-02-09 12:10:47 -0800

    Push to `gh-pages` and publish

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/hello-vite-react/',
-  plugins: [viteReact()],
-})
