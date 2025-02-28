139fa2febce8822475b8b837bad4a91d222afe39
Paul Peavyhouse <pv@swooby.com>
2025-02-28 04:33:10 -0800

    Add tailwindcss & disable its "preflight"

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/hello-vite-react/',
-  plugins: [viteReact()],
+  plugins: [
+    tailwindcss(),
+    react(),
+  ],
 })
